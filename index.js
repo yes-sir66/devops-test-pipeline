576a9156d1914b97e99b30cf2352918df0847fcb
yes-sir66 <hmaidanyasser@gmail.com>
2026-09-21 01:22:05 +0300

    'inital'

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, through training devops!");
+console.log("ci/cd pipeline is working fine");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting test...");
+setTimeout(() => {
+  console.log("test underway!");
+}, 2000);
+ console.log("test completed!");
